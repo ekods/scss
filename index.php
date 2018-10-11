3f4150e213bc320fd8f4cfa85c0a7b267f025aab
ekods <ekodwisaputro.dev@gmail.com>
2018-10-11 11:15:16 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SCSS</title>
+
+    <link rel="stylesheet" href="stylesheets/style.css">
+  </head>
+  <body>
+
+    <div id="page">
+      <div class="container">
+        <div class="banner">
+          <img src="images/SCSS-Mixins.jpg" alt="">
+        </div>
+        <section>
+          <div class="row">
+            <div class="item-2">
+              <img src="images/SCSS-Mixins.jpg" alt="">
+            </div>
+            <div class="item-2">
+              <img src="images/SCSS-Mixins.jpg" alt="">
+            </div>
+          </div>
+        </section>
+        <section>
+          <div class="row">
+            <div class="item-3">
+              <img src="images/SCSS-Mixins.jpg" alt="">
+            </div>
+            <div class="item-3">
+              <img src="images/SCSS-Mixins.jpg" alt="">
+            </div>
+            <div class="item-3">
+              <img src="images/SCSS-Mixins.jpg" alt="">
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+
+  </body>
+</html>
